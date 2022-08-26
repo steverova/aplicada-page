fa08cfe6b69450f6b65a643eeb670da64d3003a0
DESKTOP-QUP86IM\Steve <steverova0594@gmail.com>
2022-08-26 13:33:29 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/aplicada-page/precache-manifest.c7848b3e490d51bd47c4499eba744956.js"
+  "/aplicada-page/precache-manifest.5d4ab528b8572f48988027cafa075e99.js"
 );
 
 workbox.clientsClaim();
